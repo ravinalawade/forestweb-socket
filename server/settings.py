3fbf4c345a764b51ec0fd8e31c172a6d6ce479fc
ravinalawade <ravinalawade5@gmail.com>
2021-03-15 14:07:03 +0530

    some error

--- server/settings.py
+++ server/settings.py
@@ -106,7 +106,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [('*', 6379)],
         },
     },
 }
