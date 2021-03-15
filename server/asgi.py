6a68e6319658d3de02f6411ee1027fdba4dd4955
ravinalawade <ravinalawade5@gmail.com>
2021-03-15 14:25:11 +0530

    small change

--- server/asgi.py
+++ server/asgi.py
@@ -41,7 +41,7 @@ from django.core.asgi import get_asgi_application
 # Fetch Django ASGI application early to ensure AppRegistry is populated
 # before importing consumers and AuthMiddlewareStack that may import ORM
 # models.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
